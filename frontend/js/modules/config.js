7536376bd2638e565f4f2da52c85017647471301
Hunter Wilcox <hunter@hwilox.net>
2026-04-29 14:00:54 -0500

    set server url

--- frontend/js/modules/config.js
+++ frontend/js/modules/config.js
@@ -1,4 +1,4 @@
 ;(function (global) {
 	global.ResumeApp = global.ResumeApp || {}
-	global.ResumeApp.API_BASE_URL = "http://localhost:8000/api"
+	global.ResumeApp.API_BASE_URL = "http://media.judgeglass.net:8000/api"
 })(window)
